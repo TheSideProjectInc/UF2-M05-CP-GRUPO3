bf06638b32a05dab1a55a4d578cad2e4106f9587
Billy <theoneforall.90@gmail.com>
2025-04-25 19:37:54 +0200

    Implementar test para getInfo()

--- src/test/java/com/grupo3/VehiculoTest.java
+++ src/test/java/com/grupo3/VehiculoTest.java
@@ -40,8 +40,13 @@ public class VehiculoTest {
 
     @Test
     public void testGetInfo() {
-        // Test pendiente de implementación
+        Vehiculo v = new Vehiculo("Tesla", "Model S", 250, true, 5);
+        String info = v.getInfo();
+
+        assertTrue(info.contains("Tesla"));
+        assertTrue(info.contains("Model S"));
+        assertTrue(info.contains("250"));
+        assertTrue(info.contains("true"));
+        assertTrue(info.contains("5"));
     }
-
-
 }
