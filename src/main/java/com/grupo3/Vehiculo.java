7dd5695a4d29a26a4f8c0cc6a7524b8cf55f1b02
Paul-Gomez <paulgomezzubi@gmail.com>
2025-04-25 15:11:26 +0200

    Implementar lógica de acelerar y getter de velocidad actual

--- src/main/java/com/grupo3/Vehiculo.java
+++ src/main/java/com/grupo3/Vehiculo.java
@@ -8,11 +8,23 @@ public class Vehiculo {
     private boolean deportivo;
     private boolean pasajeros;
 
-    public void acelerar(int incremento) {}
+    public Vehiculo(String marca, String modelo, int velocidad_maxima) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidad_maxima = velocidad_maxima;
+        this.velocidad_actual = 0;
+    }
+
+    public void acelerar(int incremento) {
+        velocidad_actual += incremento;
+        if (velocidad_actual > velocidad_maxima) {
+            velocidad_actual = velocidad_maxima;
+        }
+    }
 
     public int getVelocidadActual() {
-        return 0;
+        return velocidad_actual;
     }
-  
 }
+
 
