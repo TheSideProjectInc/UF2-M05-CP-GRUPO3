69bb8785b49e679d1a72e2af50ec58dacdac80b8
Billy <theoneforall.90@gmail.com>
2025-04-25 11:47:16 +0200

    Estructura inicial

--- src/main/java/com/grupo3/Vehiculo.java
+++ src/main/java/com/grupo3/Vehiculo.java
@@ -3,9 +3,36 @@ package com.grupo3;
 public class Vehiculo {
     private String marca;
     private String modelo;
+
     private int velocidad_actual;
     private int velocidad_maxima;
     private boolean deportivo;
     private boolean pasajeros;
-  
+
+    public Vehiculo(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void mostrarDetalles() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+    }
+
 }
