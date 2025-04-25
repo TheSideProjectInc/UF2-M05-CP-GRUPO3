fb990e2e099ee3b88e30b70318ea4ba261a79229
yyovanyovo <80846437+yyovanyovo@users.noreply.github.com>
2025-04-25 14:24:40 +0200

    Update Main.java

--- src/main/java/com/grupo3/Main.java
+++ src/main/java/com/grupo3/Main.java
@@ -1,11 +1,1 @@
-package com.grupo3;
 
-public class Vehiculo {
-    private String marca;
-    private String modelo;
-    private int velocidad_actual;
-    private int velocidad_maxima;
-    private boolean deportivo;
-    private boolean pasajeros;
-  
-}
