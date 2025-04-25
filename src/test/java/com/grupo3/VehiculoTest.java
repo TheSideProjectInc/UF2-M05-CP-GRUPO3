b5f1c9fbb5aaf4dda74840824dbba819dcbeaed3
Rodrigo-DJB <rodrigodejuanas@gmail.com>
2025-04-25 22:01:43 +0200

    test para esMarca() planteado en su respectiva branch
    Signed-off-by: Rodrigo-DJB <rodrigodejuanas@gmail.com>

--- src/test/java/com/grupo3/VehiculoTest.java
+++ src/test/java/com/grupo3/VehiculoTest.java
@@ -1,1 +1,11 @@
+package com.grupo3;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VehiculoTest {
+    @Test
+    public void testMarcaCoincide(){
+        // test para identificar que el metodo esMarca() es correcto.
+    }
+}
