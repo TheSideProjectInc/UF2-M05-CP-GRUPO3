bc5b30e6f8186df25071f5c6bc0e252e7924d771
Rodrigo-DJB <rodrigodejuanas@gmail.com>
2025-04-25 21:57:46 +0200

    método iniciado que comparará si el valor introducido coincide con la marca del vehículo
    Signed-off-by: Rodrigo-DJB <rodrigodejuanas@gmail.com>

--- src/main/java/com/grupo3/Vehiculo.java
+++ src/main/java/com/grupo3/Vehiculo.java
@@ -7,5 +7,17 @@ public class Vehiculo {
     private int velocidad_maxima;
     private boolean deportivo;
     private boolean pasajeros;
-  
+
+    public Vehiculo(String marca, String modelo, int velocidad_maxima) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidad_maxima = velocidad_maxima;
+        this.velocidad_actual = 0;
+    }
+
+    public boolean esMarca(int marca) {
+        // metodo para comparar si la marca del vehículo coincide con el valor introducido
+    }
 }
+
+
