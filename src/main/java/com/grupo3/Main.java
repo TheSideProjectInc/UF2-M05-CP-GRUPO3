1864aea55753905248dffea2241d1a3b09d2da53
yyovanyovo <80846437+yyovanyovo@users.noreply.github.com>
2025-04-25 14:24:24 +0200

    Update Main.java
    Se agregan los atributos

--- src/main/java/com/grupo3/Main.java
+++ src/main/java/com/grupo3/Main.java
@@ -1,1 +1,11 @@
+package com.grupo3;
 
+public class Vehiculo {
+    private String marca;
+    private String modelo;
+    private int velocidad_actual;
+    private int velocidad_maxima;
+    private boolean deportivo;
+    private boolean pasajeros;
+  
+}
