633f2b60783bb82af584b63d2dc7914941163ff6
Rodrigo-DJB <rodrigodejuanas@gmail.com>
2025-04-25 21:18:33 +0200

    método frenar actualizado
    Signed-off-by: Rodrigo-DJB <rodrigodejuanas@gmail.com>

--- src/main/java/com/grupo3/Vehiculo.java
+++ src/main/java/com/grupo3/Vehiculo.java
@@ -8,12 +8,24 @@ public class Vehiculo {
     private boolean deportivo;
     private boolean pasajeros;
 
+    public Vehiculo(String marca, String modelo, int velocidad_maxima, int velocidad_actual) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidad_maxima = velocidad_maxima;
+        this.velocidad_actual = velocidad_actual;
+    }
+
     public void frenar(int decremento) {
-        if (velocidad_actual - decremento < 0) {
+        if (decremento < 0) {
+            return;
+        }else if(velocidad_actual - decremento < 0) {
             velocidad_actual = 0;
         } else {
             velocidad_actual -= decremento;
         }
     }
 
+    public int getVelocidadActual() {
+        return velocidad_actual;
+    }
 }
