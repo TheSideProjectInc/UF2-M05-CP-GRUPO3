4deb518d0c150122b028da5b0073dbad57451ec3
yyovanyovo <80846437+yyovanyovo@users.noreply.github.com>
2025-04-26 12:46:45 +0200

    Update Vehiculo.java
    Actualización constructor

--- src/main/java/com/grupo3/Vehiculo.java
+++ src/main/java/com/grupo3/Vehiculo.java
@@ -8,7 +8,7 @@ public class Vehiculo {
     private boolean deportivo;
     private boolean pasajeros;
 
-    public Vehiculo(String marca, String modelo, int velocidad_maxima) {
+    public Vehiculo(String marca, String modelo, int velocidad_maxima, boolean deportivo) {
         this.marca = marca;
         this.modelo = modelo;
         this.velocidad_maxima = velocidad_maxima;
