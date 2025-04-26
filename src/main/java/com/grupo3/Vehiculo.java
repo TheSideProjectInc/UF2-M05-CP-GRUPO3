d16e3ad77a0b3a40aabbe72cafe913bdf234c7c0
yyovanyovo <80846437+yyovanyovo@users.noreply.github.com>
2025-04-26 16:15:00 +0200

    Feature/esIgual (#17)
    * Update Vehiculo.java
    
    Se agrega la base de esIgual, falta acabar el resto.
    
    * Update Vehiculo.java
    
    Se finaliza esIgual.

--- src/main/java/com/grupo3/Vehiculo.java
+++ src/main/java/com/grupo3/Vehiculo.java
@@ -8,7 +8,7 @@ public class Vehiculo {
     private boolean deportivo;
     private boolean pasajeros;
 
-    public Vehiculo(String marca, String modelo, int velocidad_maxima, boolean deportivo) {
+  public Vehiculo(String marca, String modelo, int velocidad_maxima, boolean deportivo) {
         this.marca = marca;
         this.modelo = modelo;
         this.velocidad_maxima = velocidad_maxima;
@@ -16,11 +16,11 @@ public class Vehiculo {
         this.deportivo = deportivo;
     }
 
-    public boolean esModelo(String modelo) {
+  public boolean esModelo(String modelo) {
         return this.modelo.equalsIgnoreCase(modelo);
     }
 
-    public boolean esMarca(String marca) {
+  public boolean esMarca(String marca) {
         marca = marca.toUpperCase(); //transforma el String de la marca a comprobar en mayusculas
         this.marca = this.marca.toUpperCase(); //transforma el String de la marca del objeto vehículo a mayusculas
         if (marca.equals(this.marca)) { //compara las dos cadenas de texto en mayusuculas de la marca del vehiculo y de la marca que queremos comprobar.
@@ -29,8 +29,13 @@ public class Vehiculo {
             return false;
         }
     }
-  
-      public void frenar(int decremento) {
+    
+  public boolean esIgual(Vehiculo otro) {
+        if (otro == null) return false;
+        return this.marca.equalsIgnoreCase(otro.marca) && this.modelo.equalsIgnoreCase(otro.modelo);
+    }
+    
+  public void frenar(int decremento) {
         if (decremento < 0) {
             return; // Ignorar si decremento es negativo
         }else if(velocidad_actual - decremento < 0) {
