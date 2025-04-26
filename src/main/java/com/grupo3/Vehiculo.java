cc19d73659ec2bdd810e19413b439c2eba912b3e
Rodrigo-DJB <rodrigodejuanas@gmail.com>
2025-04-26 16:08:46 +0200

    Feature/frenar (#12)
    * Implementar método frenar
    
    * Implementar método frenar
    
    Signed-off-by: Rodrigo-DJB <rodrigodejuanas@gmail.com>
    
    * Implementar test del método frenar
    
    Signed-off-by: Rodrigo-DJB <rodrigodejuanas@gmail.com>
    
    * método frenar finalizado
    
    Signed-off-by: Rodrigo-DJB <rodrigodejuanas@gmail.com>
    
    * método frenar actualizado
    
    Signed-off-by: Rodrigo-DJB <rodrigodejuanas@gmail.com>
    
    * test para el método frenar implementados
    
    Signed-off-by: Rodrigo-DJB <rodrigodejuanas@gmail.com>
    
    ---------
    
    Signed-off-by: Rodrigo-DJB <rodrigodejuanas@gmail.com>

--- src/main/java/com/grupo3/Vehiculo.java
+++ src/main/java/com/grupo3/Vehiculo.java
@@ -7,5 +7,25 @@ public class Vehiculo {
     private int velocidad_maxima;
     private boolean deportivo;
     private boolean pasajeros;
-  
+
+    public Vehiculo(String marca, String modelo, int velocidad_maxima, int velocidad_actual) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidad_maxima = velocidad_maxima;
+        this.velocidad_actual = velocidad_actual;
+    }
+
+    public void frenar(int decremento) {
+        if (decremento < 0) {
+            return; // Ignorar si decremento es negativo
+        }else if(velocidad_actual - decremento < 0) {
+            velocidad_actual = 0; // Ajustar velocidad a 0 si sería negativa
+        } else {
+            velocidad_actual -= decremento; // Resta normal en cualquier caso válido
+        }
+    }
+
+    public int getVelocidadActual() {
+        return velocidad_actual;
+    }
 }
--- src/test/java/com/grupo3/VehiculoTest.java
+++ src/test/java/com/grupo3/VehiculoTest.java
@@ -1,1 +1,33 @@
+package com.grupo3;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VehiculoTest {
+
+    @Test
+    public void testFrenoNormal() {
+        Vehiculo vehiculo = new Vehiculo("Ford", "Focus", 100,80);
+        vehiculo.frenar(10); // velocidad_actual = 70
+        vehiculo.frenar(20); // velocidad_actual = 50
+        assertEquals(50,vehiculo.getVelocidadActual());
+    }
+
+    @Test
+    public void testFrenoNegativoIgnorado(){
+        Vehiculo vehiculo = new Vehiculo("Ford", "Focus", 100,80);
+        vehiculo.frenar(-10);
+        assertEquals(80,vehiculo.getVelocidadActual());
+    }
+
+    @Test
+    public void testFrenoMaximo(){
+        Vehiculo vehiculo = new Vehiculo("Ford", "Focus", 100,80);
+        vehiculo.frenar(20); //velocidad actual 60
+        vehiculo.frenar(20); //velocidad actual 40
+        vehiculo.frenar(10); //velocidad actual 30
+        vehiculo.frenar(40); //velocidad actual 0
+        assertEquals(0,vehiculo.getVelocidadActual());
+    }
+
+}
