71010c8d62b257dce7f5aa535a476d827bc2aedd
Billy Joshua C. Flores <87063173+TheSideProjectInc@users.noreply.github.com>
2025-04-26 12:03:24 +0200

    Update VehiculoTest.java
    Definimos el test para el método getInfo de la clase  Vehículo

--- src/test/java/com/grupo3/VehiculoTest.java
+++ src/test/java/com/grupo3/VehiculoTest.java
@@ -1,1 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class VehiculoTest {
+
+    @Test
+    public void testGetInfo() {
+        // Test pendiente de implementación
+    }
+}
 
