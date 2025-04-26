1b88d3f58d46f934997078acc417fe8416b49198
Paul-Gomez <paulgomezzubi@gmail.com>
2025-04-26 12:34:33 +0200

    Update VehiculoTest.java
    Se añade el test sin su implementación.

--- src/test/java/com/grupo3/VehiculoTest.java
+++ src/test/java/com/grupo3/VehiculoTest.java
@@ -1,1 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
+public class VehiculoTest {
+ 
+  @Test
+    void testPararVehiculo() {
+    // TODO: Implementar test para el método parar()
+  }
+
+}
