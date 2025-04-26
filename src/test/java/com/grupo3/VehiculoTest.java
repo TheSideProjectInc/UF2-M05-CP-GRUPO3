d584b8e9279fec250baa4e50870463f741d2315c
Paul-Gomez <paulgomezzubi@gmail.com>
2025-04-26 12:35:37 +0200

    Update VehiculoTest.java
    Implementación del test.

--- src/test/java/com/grupo3/VehiculoTest.java
+++ src/test/java/com/grupo3/VehiculoTest.java
@@ -5,7 +5,11 @@ public class VehiculoTest {
  
   @Test
     void testPararVehiculo() {
-    // TODO: Implementar test para el método parar()
+     Vehiculo v = new Vehiculo("Toyota", "Corolla", 120);
+        v.acelerar(80); // Simula que va a 80
+        v.parar(); // Llama al método a testear
+        assertEquals(0, v.getVelocidadActual(), "La velocidad debe ser 0 después de parar");
+
   }
 
 }
